9bd01efdb667cd9a72d05022acfd72ee64d8bbd6
Willem Allan <willemarf@gmail.com>
2013-04-03 14:04:28 -0300

    index estilo global js

--- js/global.js
+++ js/global.js
@@ -1,15 +1,79 @@
 $(function () {
 
-    $('#titulo').click(function() {
-        alert('AUDREY LINDA!!!!!!!!!!!!')
-        $('#contato').animate({
-            opacity: 0.25,
-            left: '-=50',
-            height: 'toggle'
-            }, 5000, function() {
-            // Animation complete.
-        });
+    // $(window).resize(function(){
+    //     var t = $('#home').height();
+    // });
+
+    $(document).scroll(function(){
+
+        if ( $(document).scrollTop() > $('#home').height()-2 ) {
+            $('#menu').addClass('about_menu');
+        } else {
+            $('#menu').removeClass('about_menu');
+        }
+
+        if ( $(document).scrollTop() > $('#home').height()+$('#about').height()-2 ) {
+            $('#menu').addClass('websites_menu');
+        } else {
+            $('#menu').removeClass('websites_menu');
+        }
+
+        if ( $(document).scrollTop() > $('#home').height()+$('#about').height()+$('#websites').height()-2 ) {
+            $('#menu').addClass('contact_menu');
+        } else {
+            $('#menu').removeClass('contact_menu');
+        }
+
     });
+
+    // $('#btnHome').click(function() {
+
+    //     $("#home").show();
+    //     $("#contact").hide();
+    //     $("#websites").hide();
+    //     $("#about").hide();
+
+    // });
+
+    // $('#btnAbout').click(function() {
+
+    //     $("#about").show();
+    //     $("#home").hide();
+    //     $("#contact").hide();
+    //     $("#websites").hide();
+
+    // });
+
+    // $('#btnWebsites').click(function() {
+
+    //     $("#websites").show();
+    //     $("#home").hide();
+    //     $("#about").hide();
+    //     $("#contact").hide();
+
+    // });
+
+    // $('#btnContact').click(function() {
+
+    //     $("#contact").show();
+    //     $("#home").hide();
+    //     $("#websites").hide();
+    //     $("#contact").hide();
+
+    // });
+
+
+
+    // if( $('#home').scrollTop(100) ){
+
+
+    //     console.log('aee');
+    //     $('#menu').addClass('about_menu');
+    //     // $("#menu").css({
+    //     //     'background': 'red'
+    //     // })
+
+    // }
 
 
 });
