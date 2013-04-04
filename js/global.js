c3adb064afb20ccafc22fe9cf68e95b60b11638b
Willem Allan <willemarf@gmail.com>
2013-04-04 17:49:39 -0300

    index css3 e js acertos

--- js/global.js
+++ js/global.js
@@ -4,21 +4,70 @@ $(function () {
     //     var t = $('#home').height();
     // });
 
+    $('#btnHome').on('click', function(event){
+        event.preventDefault();
+        $('#menu').addClass('home_menu');
+        $('#menu').removeClass('about_menu');
+        $('#menu').removeClass('websites_menu');
+        $('#menu').removeClass('contact_menu');
+        $('#home').addClass('select');
+        $('#about').removeClass('select');
+        $('#websites').removeClass('select');
+        $('#contact').removeClass('select');
+    });
+
+    $('#btnAbout').on('click', function(event){
+        event.preventDefault();
+        $('#menu').removeClass('home_menu');
+        $('#menu').addClass('about_menu');
+        $('#menu').removeClass('websites_menu');
+        $('#menu').removeClass('contact_menu');
+        $('#home').removeClass('select');
+        $('#about').addClass('select');
+        $('#websites').removeClass('select');
+        $('#contact').removeClass('select');
+    });
+
+    $('#btnWebsites').on('click', function(event){
+        event.preventDefault();
+        $('#menu').removeClass('home_menu');
+        $('#menu').removeClass('about_menu');
+        $('#menu').addClass('websites_menu');
+        $('#menu').removeClass('contact_menu');
+        $('#home').removeClass('select');
+        $('#about').removeClass('select');
+        $('#websites').addClass('select');
+        $('#contact').removeClass('select');
+    });
+
+    $('#btnContact').on('click', function(event){
+        event.preventDefault();
+        $('#menu').removeClass('home_menu');
+        $('#menu').removeClass('about_menu');
+        $('#menu').removeClass('websites_menu');
+        $('#menu').addClass('contact_menu');
+        $('#home').removeClass('select');
+        $('#about').removeClass('select');
+        $('#websites').removeClass('select');
+        $('#contact').addClass('select');
+    });
+
+
     $(document).scroll(function(){
 
-        if ( $(document).scrollTop() > $('#home').height()-2 ) {
+        if ( $(document).scrollTop() >= $('#home').height() ) {
             $('#menu').addClass('about_menu');
         } else {
             $('#menu').removeClass('about_menu');
         }
 
-        if ( $(document).scrollTop() > $('#home').height()+$('#about').height()-2 ) {
+        if ( $(document).scrollTop() >= $('#home').height()+$('#about').height() ) {
             $('#menu').addClass('websites_menu');
         } else {
             $('#menu').removeClass('websites_menu');
         }
 
-        if ( $(document).scrollTop() > $('#home').height()+$('#about').height()+$('#websites').height()-2 ) {
+        if ( $(document).scrollTop() >= $('#home').height()+$('#about').height()+$('#websites').height() ) {
             $('#menu').addClass('contact_menu');
         } else {
             $('#menu').removeClass('contact_menu');
