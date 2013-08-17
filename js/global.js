5693226cca6bbd5b6aef08b243cc3fe2f6305990
Willem Allan <willemarf@gmail.com>
2013-08-16 23:34:14 -0300

    otimizando js

--- js/global.js
+++ js/global.js
@@ -1,79 +1,170 @@
 $(function () {
 
-    // $(window).resize(function(){
-    //     var t = $('#home').height();
+    $('.menuItem').on('click', function(event){
+        event.preventDefault();
+
+        var paginaAtual = $(this).attr('rel');
+        var paginaAnterior = '';
+
+        paginaAnterior = location.href.split('/#')
+        paginaAnterior = paginaAnterior[1];
+
+        location.href = '#'+paginaAtual;
+
+        if( paginaAnterior && paginaAtual != paginaAnterior ){
+            $('#menu').addClass(paginaAtual + '_menu');
+            $('#'+paginaAtual).addClass('select');
+            $('#menu').removeClass(paginaAnterior + '_menu');
+            $('#'+paginaAnterior).removeClass('select');
+        }
+
+    });
+
+
+
+    // $('.pagina').each(function(){
+    //     var elemento = $(this);
+    //     alert( elemento.attr('id') );
     // });
 
-    $('#btnHome').on('click', function(event){
-        event.preventDefault();
-        $('#menu').addClass('home_menu');
-        $('#menu').removeClass('about_menu');
-        $('#menu').removeClass('websites_menu');
-        $('#menu').removeClass('contact_menu');
-        $('#home').addClass('select');
-        $('#about').removeClass('select');
-        $('#websites').removeClass('select');
-        $('#contact').removeClass('select');
-    });
+    // $('body').find('div').each(function(){  var elemento = $(this) });
 
-    $('#btnAbout').on('click', function(event){
-        event.preventDefault();
-        $('#menu').removeClass('home_menu');
-        $('#menu').addClass('about_menu');
-        $('#menu').removeClass('websites_menu');
-        $('#menu').removeClass('contact_menu');
-        $('#home').removeClass('select');
-        $('#about').addClass('select');
-        $('#websites').removeClass('select');
-        $('#contact').removeClass('select');
-    });
+    // $('#btnHome').on('click', function(event){
+    //     event.preventDefault();
 
-    $('#btnWebsites').on('click', function(event){
-        event.preventDefault();
-        $('#menu').removeClass('home_menu');
-        $('#menu').removeClass('about_menu');
-        $('#menu').addClass('websites_menu');
-        $('#menu').removeClass('contact_menu');
-        $('#home').removeClass('select');
-        $('#about').removeClass('select');
-        $('#websites').addClass('select');
-        $('#contact').removeClass('select');
-    });
+    //     var paginaAtual = 'home'
+    //     var paginaAnterior = '';
 
-    $('#btnContact').on('click', function(event){
-        event.preventDefault();
-        $('#menu').removeClass('home_menu');
-        $('#menu').removeClass('about_menu');
-        $('#menu').removeClass('websites_menu');
-        $('#menu').addClass('contact_menu');
-        $('#home').removeClass('select');
-        $('#about').removeClass('select');
-        $('#websites').removeClass('select');
-        $('#contact').addClass('select');
-    });
+    //     paginaAnterior = location.href.split('/#')
+    //     paginaAnterior = paginaAnterior[1];
+
+    //     location.href = '#'+paginaAtual;
+
+    //     $('#'+paginaAtual).addClass(paginaAtual + '_menu');
+    //     $('#'+paginaAtual).addClass('select');
+    //     if( paginaAnterior ){
+    //         $('#'+paginaAnterior).removeClass(paginaAnterior + '_menu');
+    //         $('#'+paginaAnterior).removeClass('select');
+    //     }
+
+    //     // $('#menu').addClass('home_menu');
+    //     // $('#menu').removeClass('about_menu');
+    //     // $('#menu').removeClass('websites_menu');
+    //     // $('#menu').removeClass('contact_menu');
+    //     // $('#home').addClass('select');
+    //     // $('#about').removeClass('select');
+    //     // $('#websites').removeClass('select');
+    //     // $('#contact').removeClass('select');
+    // });
 
 
-    $(document).scroll(function(){
+    // $('#btnAbout').on('click', function(event){
+    //     event.preventDefault();
 
-        if ( $(document).scrollTop() >= $('#home').height() ) {
-            $('#menu').addClass('about_menu');
-        } else {
-            $('#menu').removeClass('about_menu');
-        }
+    //     var paginaAtual = 'about'
+    //     var paginaAnterior = '';
 
-        if ( $(document).scrollTop() >= $('#home').height()+$('#about').height() ) {
-            $('#menu').addClass('websites_menu');
-        } else {
-            $('#menu').removeClass('websites_menu');
-        }
+    //     paginaAnterior = location.href.split('/#')
+    //     paginaAnterior = paginaAnterior[1];
 
-        if ( $(document).scrollTop() >= $('#home').height()+$('#about').height()+$('#websites').height() ) {
-            $('#menu').addClass('contact_menu');
-        } else {
-            $('#menu').removeClass('contact_menu');
-        }
+    //     location.href = '#'+paginaAtual;
 
-    });
+    //     $('#'+paginaAtual).addClass(paginaAtual + '_menu');
+    //     $('#'+paginaAtual).addClass('select');
+    //     if( paginaAnterior ){
+    //         $('#'+paginaAnterior).removeClass(paginaAnterior + '_menu');
+    //         $('#'+paginaAnterior).removeClass('select');
+    //     }
+
+    //     // $('#menu').removeClass('home_menu');
+    //     // $('#menu').addClass('about_menu');
+    //     // $('#menu').removeClass('websites_menu');
+    //     // $('#menu').removeClass('contact_menu');
+    //     // $('#home').removeClass('select');
+    //     // $('#about').addClass('select');
+    //     // $('#websites').removeClass('select');
+    //     // $('#contact').removeClass('select');
+    // });
+
+    // $('#btnWebsites').on('click', function(event){
+    //     event.preventDefault();
+
+    //     var paginaAtual = 'websites'
+    //     var paginaAnterior = '';
+
+    //     paginaAnterior = location.href.split('/#')
+    //     paginaAnterior = paginaAnterior[1];
+
+    //     location.href = '#'+paginaAtual;
+
+    //     $('#'+paginaAtual).addClass(paginaAtual + '_menu');
+    //     $('#'+paginaAtual).addClass('select');
+    //     if( paginaAnterior ){
+    //         $('#'+paginaAnterior).removeClass(paginaAnterior + '_menu');
+    //         $('#'+paginaAnterior).removeClass('select');
+    //     }
+
+    //     // $('#menu').removeClass('home_menu');
+    //     // $('#menu').removeClass('about_menu');
+    //     // $('#menu').addClass('websites_menu');
+    //     // $('#menu').removeClass('contact_menu');
+    //     // $('#home').removeClass('select');
+    //     // $('#about').removeClass('select');
+    //     // $('#websites').addClass('select');
+    //     // $('#contact').removeClass('select');
+    // });
+
+    // $('#btnContact').on('click', function(event){
+    //     event.preventDefault();
+
+    //     var paginaAtual = 'contact'
+    //     var paginaAnterior = '';
+
+    //     paginaAnterior = location.href.split('/#')
+    //     paginaAnterior = paginaAnterior[1];
+
+    //     location.href = '#'+paginaAtual;
+
+
+    //     $('#menu').addClass(paginaAtual + '_menu');
+    //     $('#'+paginaAtual).addClass('select');
+    //     if( paginaAnterior ){
+    //         $('#menu').removeClass(paginaAnterior + '_menu');
+    //         $('#'+paginaAnterior).removeClass('select');
+    //     }
+
+    //     // $('#menu').removeClass('home_menu');
+    //     // $('#menu').removeClass('about_menu');
+    //     // $('#menu').removeClass('websites_menu');
+    //     // $('#menu').addClass('contact_menu');
+    //     // $('#home').removeClass('select');
+    //     // $('#about').removeClass('select');
+    //     // $('#websites').removeClass('select');
+    //     // $('#contact').addClass('select');
+    // });
+
+
+    // $(document).scroll(function(){
+
+    //     if ( $(document).scrollTop() >= $('#home').height() ) {
+    //         $('#menu').addClass('about_menu');
+    //     } else {
+    //         $('#menu').removeClass('about_menu');
+    //     }
+
+    //     if ( $(document).scrollTop() >= $('#home').height()+$('#about').height() ) {
+    //         $('#menu').addClass('websites_menu');
+    //     } else {
+    //         $('#menu').removeClass('websites_menu');
+    //     }
+
+    //     if ( $(document).scrollTop() >= $('#home').height()+$('#about').height()+$('#websites').height() ) {
+    //         $('#menu').addClass('contact_menu');
+    //     } else {
+    //         $('#menu').removeClass('contact_menu');
+    //     }
+
+    // });
 
     // $('#btnHome').click(function() {
 
